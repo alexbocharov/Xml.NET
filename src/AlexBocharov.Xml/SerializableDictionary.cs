fe37b7bc7c3335a4feed80dec33946f791c98eff
Alexander Bocharov <alex.bocharov@outlook.com>
2024-05-27 14:48:52 +0300

    Initial project setup with GitHub Actions, .editorconfig, and .gitattributes
    This commit includes the initial setup of the project. Added .editorconfig and .gitattributes files to define coding style and Git behavior respectively. GitHub Actions workflows for building and publishing the project are defined in build.yml and publish.yml. Updated .gitignore to ignore unnecessary files. Added Visual Studio solution file AlexBocharov.Xml.sln, and common properties for all projects are defined in Directory.Build.props. NuGet package versions are managed centrally in Directory.Packages.props. Added project files for AlexBocharov.Xml and AlexBocharov.Xml.Tests, along with their respective classes and unit tests.

---
+++ src/AlexBocharov.Xml/SerializableDictionary.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Alexander Bocharov.
+// Licensed under the MIT License. See the LICENSE file in the project root for more information.
+
+using System.Xml.Schema;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace AlexBocharov.Xml;
+
+/// <summary>
+/// Represents a serializable dictionary.
+/// </summary>
+/// <seealso cref="IXmlSerializable" />
+/// <remarks>
+/// This class is used to serialize and deserialize dictionaries to and from XML.
+/// </remarks>
+public sealed class SerializableDictionary : Dictionary<string, string?>, IXmlSerializable
+{
+    /// <inheritdoc />
+    public XmlSchema? GetSchema() => null;
+
+    /// <inheritdoc />
+    public void ReadXml(XmlReader reader)
+    {
+        bool wasEmpty = reader.IsEmptyElement;
+        reader.Read();
+
+        if (wasEmpty)
+        {
+            return;
+        }
+
+        while (reader.NodeType != XmlNodeType.EndElement)
+        {
+            if (reader.NodeType != XmlNodeType.Element)
+            {
+                reader.Read();
+                continue;
+            }
+
+            string key = reader.Name;
+            string? value = reader.ReadElementContentAsString();
+            Add(key, value);
+        }
+
+        reader.ReadEndElement();
+    }
+
+    /// <inheritdoc />
+    public void WriteXml(XmlWriter writer)
+    {
+        foreach (var kvp in this.Where(kvp => kvp.Value is not null))
+        {
+            writer.WriteElementString(kvp.Key, kvp.Value);
+        }
+    }
+}
